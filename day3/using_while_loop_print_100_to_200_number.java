859ccc24f650da4171e3c2a507fec0956ea77fe1
SANDEEP KUMAR <113803494+Sandeepguptask3@users.noreply.github.com>
2022-10-01 22:12:40 +0530

    Create using_while_loop_print_100_to_200_number.java

---
+++ day3/using_while_loop_print_100_to_200_number.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class using_while_loop_print_100_to_200_number {
+    public static void main(String[] args) {
+        int i = 100;
+        while(i<=200){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
